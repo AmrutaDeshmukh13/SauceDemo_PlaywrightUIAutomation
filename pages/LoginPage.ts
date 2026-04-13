db303923dd5c2cb802c2a5993afb89bf712b4907
Amruta <amrujagtap@gmail.com>
2026-04-13 22:51:47 +0530

    yml updated

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -5,16 +5,18 @@ export class LoginPage {
     readonly username: Locator;
     readonly password: Locator;
     readonly loginButton: Locator;
-
+   
     constructor(page: Page) {
         this.page = page;
         this.username = page.locator('#user-name');
         this.password = page.locator('#password');
         this.loginButton = page.locator('#login-button');
+        
     }
 
     async gotoLoginPage() {
         await this.page.goto(`${process.env.APP_URL}`);
+        
     }
 
     async login() {
