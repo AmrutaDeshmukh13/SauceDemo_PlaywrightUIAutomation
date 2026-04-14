2607c81adc0bee356a33d7334decf10a1a1ec920
AmrutaDeshmukh13 <amrujagtap@gmail.com>
2026-04-14 17:35:53 +0530

    Simplify dotenv configuration in playwright.config.ts
    Remove specific path for dotenv configuration.

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,8 +7,8 @@ import { defineConfig, devices } from '@playwright/test';
  import dotenv from 'dotenv';
  
  import path from 'path';
- dotenv.config({ path: path.resolve(__dirname, '.env') });
-
+// dotenv.config({ path: path.resolve(__dirname, '.env') });
+ dotenv.config();
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
